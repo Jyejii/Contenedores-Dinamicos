0712ced626aa4da7bcaa4d335906a287ee1a767f
Jiachen <jye@myuax.com>
2023-11-30 10:50:20 +0100

    Ej terminado

--- Ej1.cpp
+++ Ej1.cpp
@@ -4,3 +4,12 @@
 int main() {
     std::vector<int> numeros;
     numeros.push_back(10), numeros.push_back(20), numeros.push_back(30), numeros.push_back(40), numeros.push_back(50);
+
+    std::cout << "Elementos del vector:" << std::endl;
+    for (std::vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
