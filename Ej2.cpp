d450f5813a0d6eb2495ad7d3d82bb817af724acc
Jiachen <jye@myuax.com>
2023-11-30 11:07:15 +0100

    .

--- Ej2.cpp
+++ Ej2.cpp
@@ -3,3 +3,9 @@
 #include <stack>
 int main() {
     std::list<double> numeros = {1.5, 2.7, 3.8, 4.2, 5.1};
+    std::stack<double, std::list<double>> pila(numeros);
+    std::cout << "Pila inicial:" << std::endl;
+    while (!pila.empty()) {
+        std::cout << pila.top() << " ";
+        pila.pop();
+    }
