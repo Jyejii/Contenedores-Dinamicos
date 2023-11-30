6ffd4f3c292f1dfdf31797feb0400d2e2d5e7e62
Jiachen <jye@myuax.com>
2023-11-30 17:26:57 +0100

    ej 4 cambio de nombre

--- Ej6.cpp
+++ Ej6.cpp
@@ -1,3 +1,15 @@
-//
-// Created by jiach on 30/11/2023.
-//
+#include <iostream>//NO ME SALIO ESTE EJERCICIO Y NO HE PUSTO EL CMAKELIST PORQUE SINO EL RESTO DE EJERCICIOS DEJAN DE FUNCIONAR
+#include <boost/lexical_cast.hpp>
+
+int main() {
+    std::string s = "12345";
+
+    try {
+        int i = boost::lexical_cast<int>(s);
+        std::cout << "Número: " << i << std::endl;
+    } catch(boost::bad_lexical_cast &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
